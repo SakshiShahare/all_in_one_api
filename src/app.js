8475ad2a7cb06debcb5e84e048d363d9c8a4928f
Sakshi <sakshi@Sakshis-MacBook-Pro.local>
2024-03-22 11:34:37 +0530

    asyncHandler added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
 
 const app = express();
+//to make specific frontend talk to the backend
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
+}))
+
+//to parse json data
+app.use(express.json({limit : "16kb"}))
+
+//to parse url data
+app.use(express.urlencoded({extended : true}))
 
 export default app;
