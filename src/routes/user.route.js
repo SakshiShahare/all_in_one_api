fec54216afff79c792d53bf234526e85c714429a
Sakshi <sakshi@Sakshis-MacBook-Pro.local>
2024-03-25 18:52:11 +0530

    all the user routes are set

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import Router from "express"
-import {loginUser, logoutUser, refreshAccessToken, registerUser} from "../controllers/user.controller.js"
+import {changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateUserAvatar, updateUserCoverImage} from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyToken } from "../middlewares/auth.middleware.js";
 
@@ -27,6 +27,13 @@ router.route('/login').post(loginUser);
 router.route('/logout').post(verifyToken , logoutUser);
 //not need to use middleware all the logic is decoded in the function only
 router.route('/refresh-token').post(refreshAccessToken);
+router.route('/change-password').post(verifyToken , changeCurrentPassword);
+router.route('/current-user').get(verifyToken , getCurrentUser);
+router.route('/update-details').patch(verifyToken , updateAccountDetails);
+router.route('/avatar-update').patch(verifyToken , upload.single("avatar") , updateUserAvatar)
+router.route('/coverImage-update').patch(verifyToken , upload.single("coverImage") , updateUserCoverImage)
+router.route('/c/:username').get(verifyToken , getUserChannelProfile);
+router.route('/watchHistory').get(verifyToken , getWatchHistory);
 
 
 export default router;
